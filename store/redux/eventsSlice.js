4874f085f839103594a78b3f889464991a281846
Ezequiel Garcia <eze.hoch@gmail.com>
2022-08-14 20:50:17 +0300

    events store

--- store/redux/eventsSlice.js
+++ store/redux/eventsSlice.js
@@ -14,37 +14,13 @@ const eventsSlice = createSlice({
       state.items = action.payload.items;
     },
     addEvent(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id);
-      state.totalQuantity++;
-      state.changed = true;
-      if (!existingItem) {
-        state.items.push({
-          id: newItem.id,
-          price: newItem.price,
-          quantity: 1,
-          totalPrice: newItem.price,
-          name: newItem.title,
-        });
-      } else {
-        existingItem.quantity++;
-        existingItem.totalPrice += newItem.price;
-      }
+      state.events = [action.payload, ...state.events];
     },
     removeEvent(state, action) {
-      const id = action.payload;
-      const existintItem = state.items.find((item) => item.id === id);
-      state.totalQuantity--;
-      state.changed = true;
-      if (existintItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id !== id);
-      } else {
-        existintItem.quantity--;
-        existintItem.totalPrice -= existintItem.price;
-      }
+      state.events = state.events.filter((item) => item.id !== id);
     },
   },
 });
 
-export const eventsActions = eventsSlice.actions;
+export const { replaceCart, addEvent, removeEvent } = eventsSlice.actions;
 export default eventsSlice;
--- store/redux/index.js
+++ store/redux/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import eventsSlice from './eventsSlice';
 
 // import uiSlice from './ui-slice';
 // import cartSlice from './cart-slice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: { events: eventsSlice.reducer },
 });
 
 export default store;
